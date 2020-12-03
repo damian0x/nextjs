2f08ec79090d2d2efbd2690358c5408a53170018
Damian Janus <damian.janus@cofinity.io>
2020-12-03 11:28:48 +0100

    meta

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,9 @@ export default function Home() {
     <div>
       <Head>
         <title>New Adventure</title>
+        <meta name="description" 
+content="This is an example of a meta description. 
+This will often show up in search results." />
         <link rel="icon" href="/favicon.ico" />
         <link defer rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
