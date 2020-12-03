a6ca2d05b50d1f0f54304de538eb250aecc1e0e7
Damian Janus <damian.janus@cofinity.io>
2020-12-03 11:03:18 +0100

    f5

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,8 +9,6 @@ export default function Home() {
         <title>New Adventure</title>
         <link rel="icon" href="/favicon.ico" />
         <link defer rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
 
       <main>
